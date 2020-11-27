f49e92982bd87542d1897835f09fb7141bcd5e52
nemo-mobile <guilherme.o.peixe@gmail.com>
2020-11-26 22:52:12 -0300

    estrutura do projeto alterada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const cors = require("cors")
-
 const app = express();
-const summonerRoute = require("./routes/summoner/summoner.js")
-const championRoute = require("./routes/champion/champion.js")
+const routes = require("./routes/routes.js")
 
 app.use(cors())
-
-app.listen(3000, console.log("Listening on 8080"))
-
-app.get("/", (req, res) => {
-    res.send("Home Page")
-})
-
-app.use("/summoner", summonerRoute)
-app.use("/champion", championRoute)
+app.listen(3000, console.log("Listening on 3000"))
+app.use("/", routes);
