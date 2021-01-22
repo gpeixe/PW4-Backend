9f5255a0398fa1b0b9e9ce87d82c5afa0b94ae3a
nemo-mobile <guilherme.o.peixe@gmail.com>
2021-01-21 22:30:22 -0300

    Mudancas championController

--- src/controllers/championController.js
+++ src/controllers/championController.js
@@ -3,10 +3,11 @@ const axios = require("axios");
 exports.get_champion_by_name = async function (req, res){
     const { championName } = req.params
 
-    const champion = await axios.get(`http://ddragon.leagueoflegends.com/cdn/10.21.1/data/pt_BR/champion/${championName}.json`)
-        .then(resp => Object.values(resp.data.data).pop())
+    let champion = await axios.get(`http://ddragon.leagueoflegends.com/cdn/10.21.1/data/pt_BR/champion/${championName}.json`)
+        .then(resp => resp.data.data)
         .catch(err => err)
 
+   champion = champion[championName]
 
    champion['skins'].forEach( skin => {
       skin['skinImage'] = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name}_${skin.num}.jpg`
@@ -14,26 +15,26 @@ exports.get_champion_by_name = async function (req, res){
 
    champion['tileImage'] = `http://ddragon.leagueoflegends.com/cdn/img/champion/tiles/${champion.name}_0.jpg`
    champion['splashImage'] = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name}_0.jpg`
-    
+
    res.json(champion)
 }
 
 
 exports.get_all_champions = async function (req, res){
-    const championsObject = await axios.get(`http://ddragon.leagueoflegends.com/cdn/10.21.1/data/pt_BR/champion.json`)
-        .then(resp => resp.data.data)
+    const champions = await axios.get(`http://ddragon.leagueoflegends.com/cdn/10.21.1/data/pt_BR/champion.json`)
+        .then(resp => Object.values(resp.data.data))
         .catch(err => err)
 
-    const championsArray = Object.values(championsObject)
-
-    const championsWithCardImage = championsArray.map((champion) => {
-      const name = champion.name
-
+    const championsDTO = champions.map((champion) => {
       return {
-        champion,
-        image: `http://ddragon.leagueoflegends.com/cdn/img/champion/tiles/${name}_0.jpg`,
+        name: champion.name,
+        key: champion.key,
+        image: `http://ddragon.leagueoflegends.com/cdn/img/champion/tiles/${champion.name}_0.jpg`,
+        title: champion.title,
+        blurb: champion.blurb,
+        tags: champion.tags
       }
-  })
+    })
 
-    res.json(championsWithCardImage)
+    res.json(championsDTO)
 }
